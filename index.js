b6a4cda7c2e1caff84ddb611238a9d0cabb5968c
bikrantbdr <bikrantbdr@gmail.com>
2024-03-06 22:50:10 +0545

    env file setup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(cors());
 
@@ -10,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5000/",
+    origin: process.env.HOST,
     methods: ["GET", "POST"],
     credentials: true,
   },
